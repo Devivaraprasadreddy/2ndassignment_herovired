93df7c411323b83ea4ff547cf65e064bf1ee8fcf
DEVI VARA PRASAD REDDY KADIMISETTI <devivaraprasad55@gmail.com>
2022-12-19 22:04:49 +0530

    New Assignment

--- p1.py
+++ p1.py
@@ -17,34 +17,34 @@ from tabulate import tabulate
 import pandas as pd
 # entries = {}
 
-n1=int(input("Enter A Number 1 : "))
+user1=int(input("Enter A Number 1 : "))
 
-n2=int(input("Enter A Number 2 : "))
+user2=int(input("Enter A Number 2 : "))
 
-n3=int(input("Enter A Number 3 : "))
+user3=int(input("Enter A Number 3 : "))
 
-n4=int(input("Enter A Number 4 : "))
+user4=int(input("Enter A Number 4 : "))
 
-n5=int(input("Enter A Number 5 : "))
+user5=int(input("Enter A Number 5 : "))
 
-if(n1<=0 or n2<=0 or n3<=0 or n4<=0 or n5<=0):
+if(user1<=0 or user2<=0 or user33<=0 or user4<=0 or user5<=0):
 
     print("Please Enter A positive Number!")
 
 else:
-    total1 = n1*n1
+    total1 = user1*user1
 
-    total2 = n2*n2  
+    total2 = user2*user2  
 
-    total3 = n3*n3  
+    total3 = user3*user3  
 
-    total4 = n4*n4  
+    total4 = user4*user4  
 
-    total5 = n5*n5 
+    total5 = user5*user5 
 
     entries = {
 
-        "SERIES" : [n1,n2,n3,n4,n5],
+        "SERIES" : [user1,user2,user3,user4,user5],
 
         "OUTPUTS" : [total1,total2,total3,total4,total5]
     }
